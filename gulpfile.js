c93a184da485e81b7a2478b6c73909c730fa691a
Ketilen Sagas <ketilensagas@hotmail.com>
2024-05-04 19:12:06 -0300

    minificando css e imagens

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const image = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin')
 const stripJs = require('gulp-strip-comments')
 const stripCss = require('gulp-strip-css-comments')
-const htmlmin= require('gulp-htmlmin')
-const {series, parallel}= require('gulp')
-const babel= require('gulp-babel')
-const browserSync= require('browser-sync').create()
-const reload= browserSync.reload
+const htmlmin = require('gulp-htmlmin')
+const { series, parallel } = require('gulp')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 
 function tarefasCSS(callback) {
@@ -20,12 +20,12 @@ function tarefasCSS(callback) {
         './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
         './vendor/owl/css/owl.css',
         './vendor/jquery-ui/jquery-ui.css',
-        './src/css/style.css'
+        'src/css/style.css'
     ])
         .pipe(babel({
             comments: false,
             presets: ['@babel/env']
-        }))                   
+        }))
 
         .pipe(concat('styles.css'))         // mescla arquivos
         .pipe(cssmin())                     // minifica css
@@ -49,25 +49,18 @@ function tarefasJS(callback) {
         .pipe(uglify())                     // minifica js
         .pipe(rename({ suffix: '.min' }))    // scripts.min.js
         .pipe(gulp.dest('./dist/js')) // cria arquivo em novo diretório
-    return callback()               
+    return callback()
 }
 
-function tarefasImagem() {
+function tarefasImagem(cb) {
 
-    return gulp.src('.src/images/*.jpg', '.src/images/*.png')
-        .pipe(image({
-            pngquant: true,
-            optipng: false,
-            zopflipng: true,
-            jpegRecompress: false,
-            mozjpeg: true,
-            gifsicle: true,
-            svgo: true,
-            concurrent: 10,
-            quiet: true
-        }))
-        .pipe(gulp.dest('.dist/images'))
+    gulp.src('src/**/*')
+        .pipe(imagemin({ verbose: true }))
+        .pipe(gulp.dest('dist'));
+
+    cb();
 }
+
 
 function tarefasHTML(callback) {
     gulp.src('./src/**/*.html')
@@ -76,7 +69,7 @@ function tarefasHTML(callback) {
     return callback()
 }
 
-gulp.task('serve', function(){
+gulp.task('serve', function () {
 
     browserSync.init({
         server: {
@@ -85,11 +78,11 @@ gulp.task('serve', function(){
     })
 
     gulp.watch('./dist/**/*').on('change', reload)
-    gulp.watch('./src/**/*').on('change',process) // repete quando for alterar src
+    gulp.watch('./src/**/*').on('change', process) // repete quando for alterar src
 })
 
 
-const process= series(tarefasHTML, tarefasJS, tarefasCSS)
+const process = series(tarefasHTML, tarefasJS, tarefasCSS)
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
